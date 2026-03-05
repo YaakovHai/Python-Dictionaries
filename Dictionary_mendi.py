b7b18fd403b104c971cb23abff08edfa0e746afb
Mendl-M <mendi20002@gmail.com>
2026-03-05 20:54:55 +0200

    added dictionary

---
+++ Dictionary_mendi.py
@@ -0,0 +1,14 @@
+person = {
+    "name": "mendi",
+    "age": 23,
+    "course": "Python",
+    "job" : "Developer",
+    "salary" : "15000",
+
+}
+# print(person["name"])
+# print(person["age"])
+# print(person["course"])
+# print(person["job"])
+print(person["salary"])
+
