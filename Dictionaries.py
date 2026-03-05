90391d30bdb5dcc0f8fb2df19d4b091b7b82e3eb
YaakovHai <kobi8471@gmail.com>
2026-03-05 20:54:16 +0200

    start

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,5 +1,15 @@
-student = {
-    "name": "David",
-    "age": 25,
-    "course": "Python"
+# person = {
+#     "name": "mendi",
+#     "age": 23,
+#     "course": "python"
+# }
+
+
+person = {
+    "name": "mendi",
+    "age": 23,
+    "course": "python"
 }
+print(person["name"])
+print(person["age"])
+print(person["course"])
