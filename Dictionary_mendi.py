0e29641854c9f4c665c5e6d12e9f5fb363968e66
Mendl-M <mendi20002@gmail.com>
2026-03-05 21:36:25 +0200

    added conditions

--- Dictionary_mendi.py
+++ Dictionary_mendi.py
@@ -4,13 +4,27 @@ person = {
     "course": "Python",
     "job" : "Developer",
     "salary" : "15000",
-
+    "city" : "Tel aviv"
 }
-# print(person["name"])
-# print(person["age"])
-# print(person["course"])
-# print(person["job"])
+
 person["age"] = 35
 print("Update age:", person["age"] )
+
 person["job"] = "Senior Developer"
 print("Update job:", person["job"])
+
+person["salary"] = "20000"
+print("Update salary:", person["salary"])
+
+person["city"] = "Haifa"
+print("The new city:", person["city"])      
+
+# בדיקת קיום של מפתח במילון
+if "age" in person:
+    print("Age is in the dictionary.")
+else:    print("Age is not in the dictionary.") 
+
+if "phone" in person:
+    print("Phone is in the dictionary.")        
+else:    print("Phone is not in the dictionary.") 
+
