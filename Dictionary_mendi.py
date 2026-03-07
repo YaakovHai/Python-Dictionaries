b40b5ea9db445ef8ec564750851cbfc4c264f755
Mendl-M <mendi20002@gmail.com>
2026-03-07 21:10:07 +0200

    end ecercise

--- Dictionary_mendi.py
+++ Dictionary_mendi.py
@@ -1,3 +1,4 @@
+
 person = {
     "name": "mendi",
     "age": 23,
@@ -28,3 +29,45 @@ if "phone" in person:
     print("Phone is in the dictionary.")        
 else:    print("Phone is not in the dictionary.") 
 
+for key, value in person.items():    
+    print(f" {key} : {value}")     
+
+server = {
+    "hostname":"mylinux",
+    "ip" : "127.0.0.1",
+    "os" : "Ubuntu",
+    "cpu_cores" : "100%cpu",
+    "status" : "running",
+}
+
+print(server["status"])
+
+users = [
+    {"name": "David", "age": 30},
+    {"name": "Moshe", "age": 16},
+    {"name": "Noa", "age": 49},
+    
+]
+
+# for i in users:
+#     if i ["age"] > 30:
+#      print(f"User over 30: {i["name"]} age: {i["age"]}")
+
+users.append({"name": "Noa", "age": 22},)
+
+# print("---Updated User List ---")
+# for user in users:
+#     print(user)
+
+for i in users:
+    print(f"{i["name"]} is {i["age"]} years old")
+
+inventory = {
+    "name" : "banana",
+    "price" : 3500,
+    "quantity" : 3,
+}
+
+total_price = inventory["price"] * inventory["quantity"]
+
+print(f"The total price for {inventory['name']} : {total_price}")
